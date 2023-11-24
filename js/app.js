e94f9a4d6cacc313345c036e558a67bd6f6b25eb
Fajril Yusin Supriadi <karinablood1@gmail.com>
2023-11-24 19:16:10 +0700

    mencoba 2

--- js/app.js
+++ js/app.js
@@ -4,8 +4,17 @@ const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
 
+const ml_section = document.querySelector(".milestones");
+const ml_counters = document.querySelectorAll(".number span");
+
+const links = document.querySelectorAll(".nav-link");
+
+const hamburger = document.querySelector(".hamburger");
+
 window.addEventListener("scroll", () => {
+  activeLink();
   if (!skillsPlayed) skillsCounter();
+  if (!mlPlayed) mlCounter();
 });
 
 // Sticky Nav
@@ -70,3 +79,63 @@ function skillsCounter() {
     (p) => (p.style.animation = "progress 2s ease-in-out forwards")
   );
 }
+
+// Services Counter Animation
+
+let mlPlayed = false;
+
+function mlCounter() {
+  if (!hasReached(ml_section)) return;
+  mlPlayed = true;
+  ml_counters.forEach((ctr) => {
+    let target = +ctr.dataset.target;
+
+    setTimeout(() => {
+      updateCount(ctr, target);
+    }, 400);
+  });
+}
+
+// Portfolio Filter Animation
+
+let mixer = mixitup(".portfolio-gallery", {
+  selectors: {
+    target: ".prt-card",
+  },
+  animation: {
+    duration: 500,
+  },
+});
+
+// Change Active Link On Scroll
+
+function activeLink() {
+  let sections = document.querySelectorAll("section[id]");
+  let passedSections = Array.from(sections)
+    .map((sct, i) => {
+      return {
+        y: sct.getBoundingClientRect().top - header.offsetHeight,
+        id: i,
+      };
+    })
+    .filter((sct) => sct.y <= 0);
+
+  let currSectionID = passedSections.at(-1).id;
+
+  links.forEach((l) => l.classList.remove("active"));
+  links[currSectionID].classList.add("active");
+}
+
+//  Open & Close Navbar Menu
+
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("open");
+  // document.body.classList.toggle("stopScrolling");
+});
+
+// links.forEach((link) =>
+//   link.addEventListener("click", () => {
+//     document.body.classList.remove("open");
+//     document.body.classList.remove("stopScrolling");
+//   })
+// );
